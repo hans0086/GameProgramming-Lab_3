24285e185f288207513af13ce1c54130d687ce39
hans0086 <hans0086@algonquinlive.com>
2016-02-08 21:22:52 -0500

    Finished Lab 3

---
+++ Lab_3/Assets/ChangeButtonText.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeButtonText : MonoBehaviour {
+
+	// Button we are going to change the text of.
+	public Button button;
+	string buttonKey = "INITIAL_BUTTON_TEXT_VALUE";
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey (buttonKey)) {
+			Text buttonText = button.GetComponentInChildren<Text>();
+			buttonText.text = PlayerPrefs.GetString (buttonKey);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeText() {
+		if(button) {
+			// Gets the Text child object of the button so that we can set its text.
+			Text buttonText = button.GetComponentInChildren<Text>();
+			switch(buttonText.text){
+			case "OFF":
+				buttonText.text = "ON";
+				PlayerPrefs.SetString (buttonKey, buttonText.text);
+				PlayerPrefs.Save ();
+				break;
+			case "ON":
+				buttonText.text = "OFF";
+				PlayerPrefs.SetString (buttonKey, buttonText.text);
+				PlayerPrefs.Save ();
+				break;
+			default:
+				break;
+			}
+
+		}
+	}
+}
---
+++ Lab_3/Assets/SaveToggle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SaveToggle : MonoBehaviour {
+	public Toggle toggle;
+	string toggleKey = "INITIAL_TOGGLE_VALUE";
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey (toggleKey)) {
+			//if the toggleKey exists and is set to 1
+			if (PlayerPrefs.GetInt (toggleKey) == 1) {
+				//check the toggle
+				toggle.isOn = true;
+			} 
+			else {
+				//uncheck the toggle
+				toggle.isOn = false;
+			}
+		} 
+		else {
+			saveToggleValue ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void saveToggleValue(){
+		if (toggle.isOn == true) {
+			//create case for "checked" state
+			PlayerPrefs.SetInt (toggleKey, 1);
+		} else {
+			//create case for "unchecked" state
+			PlayerPrefs.SetInt (toggleKey, 0);
+		}
+		//save the current state
+		PlayerPrefs.Save ();
+	}
+}
